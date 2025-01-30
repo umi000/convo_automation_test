b545b20b3b6ba626fcc32301043497ee0c20d59b
mmoizulhaque <94143721+mmoizulhaque@users.noreply.github.com>
2025-01-30 19:27:54 +0500

    changes for pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,23 @@
 const { defineConfig } = require("cypress");
-const { connectToDatabase, fetchFirstSortedRecord } = require("./cypress/helpers/dbHelper");
-require('dotenv').config();
+const {
+  connectToDatabase,
+  fetchFirstSortedRecord,
+} = require("./cypress/helpers/dbHelper");
+require("dotenv").config();
 const config = {
-  mongoUri: process.env.Mongo_URI
-}
+  mongoUri: process.env.Mongo_URI,
+};
 module.exports = config;
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://staging.stablestakesiq.com/',
+    baseUrl: "https://staging.stablestakesiq.com/",
+    video: true,
+    screenshotOnRunFailure: true,
+    viewportWidth: 1280,
+    viewportHeight: 720,
     setupNodeEvents(on, config) {
-      on('task', {
+      on("task", {
         connectToDb: async () => {
           try {
             await connectToDatabase();
@@ -22,22 +29,25 @@ module.exports = defineConfig({
         fetchFirstSortedRecord: async () => {
           try {
             const record = await fetchFirstSortedRecord(
-              'equibase_entries',
-              'createdAt',
+              "equibase_entries",
+              "createdAt",
               -1,
-              ['postTime', 'raceDate', 'trackId', 'createdAt']
+              ["postTime", "raceDate", "trackId", "createdAt"]
             );
             // await fetchFirstSortedRecord();
             return record;
           } catch (error) {
             return error.message;
           }
-        }
+        },
       });
       return config;
     },
   },
   env: {
-    MAILOSAUR_API_KEY: "5C3WEUikZciIelzMCDneChNytU0YDSRN",
-  }
+    dev: "https://develop.stablestakesiq.com/",
+    staging: "https://staging.stablestakesiq.com/",
+    beta: "https://beta.stablestakesiq.com/",
+    prod: "https://app.stablestakesiq.com/",
+  },
 });
